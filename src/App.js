e45d0431ac1aa62014ad38957a706f7ad309d5d1
Sven Petermanec <epsteve@ericsson.com>
2021-09-10 18:27:09 +0200

    Finished refactoring to hooks and implemented context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navabr from './Components/Layout/Navbar';
-import Users from './Components/Users/Users';
 import User from './Components/Users/User';
-import Search from './Components/Users/Search';
+import Home from './Components/Pages/Home';
+import NotFound from './Components/Pages/NotFound';
 import Alert from './Components/Layout/Alert';
 import About from './Components/Pages/About';
 import GithubState from './context/github/GithubState';
@@ -20,18 +20,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
-                <Route exact path="/user/:login" component={User}></Route>
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
