d6897fa97cf35f7ba2de1be4485cf3bbc832983d
Sven Petermanec <epsteve@ericsson.com>
2021-09-10 18:40:32 +0200

    App level state moved to context

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Search from './../Users/Search';
+import Users from '../Users/Users';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Search />
+      <Users />
+    </Fragment>
+  );
+};
+
+export default Home;
